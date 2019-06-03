6f1822e5f626c909c13727b490a2693c428a2d27
francis <francis@francisdu.com>
2019-06-03 11:56:22 +0800

    update

---
+++ src/main/java/com/acquisition/config/YiliPoolConfig.java
@@ -0,0 +1,40 @@
+package com.acquisition.config;
+
+import com.acquisition.entity.CjDataSourceConnDefine;
+import com.acquisition.job.DatabaseType;
+import com.acquisition.service.ICjDataSourceConnDefineService;
+import com.yili.pool.pool.GroupPoolFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author yxk
+ * @Description: 分组连接池配置
+ * @create 2019/6/2
+ * @since 1.0.0
+ */
+@Component
+public class YiliPoolConfig implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(YiliPoolConfig.class);
+
+    @Resource(name = "cjDataSourceConnDefineServiceImpl")
+    ICjDataSourceConnDefineService iCjDataSourceConnDefineService;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<CjDataSourceConnDefine> allconninfo = iCjDataSourceConnDefineService.selectByExample();
+        for (CjDataSourceConnDefine dta : allconninfo) {
+            GroupPoolFactory groupPoolFactory = GroupPoolFactory.getInstance(dta.getBusinessSystemNameShortName() + dta.getDataSourceSchema());
+            groupPoolFactory.setConfigurationParameter(DatabaseType.AdapterDatabaseType(dta.getDataBaseType()), "", dta.getConnIp(), dta.getConnPort(), dta.getDataSourceSchema(), dta.getLoginName(), dta.getLoginPassword());
+        }
+        LOG.info("YILIGroupPool创建成功");
+    }
+
+}
---
+++ src/main/java/com/acquisition/job/DatabaseType.java
@@ -0,0 +1,36 @@
+package com.acquisition.job;
+
+import com.yili.pool.util.JdbcPojo;
+import com.yili.pool.util.PoolDataSource;
+
+/**
+ * @author yxk
+ * @Description: 适配Database
+ * @create 2019/5/30
+ * @since 1.0.0
+ */
+public class DatabaseType {
+
+    /**
+     * 匹配各种数据库
+     */
+    public static String AdapterDatabaseType(String par) {
+        String str = "";
+        switch (TypeEnume.getDatabaseName(par)) {
+
+            case MYSQL:
+                str = "com.mysql.jdbc.Driver";
+                break;
+            case SQLSERVER:
+                str = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+                break;
+            case ORACLE:
+                str = "oracle.jdbc.OracleDriver";
+                break;
+            case HIVE:
+                str = "org.apache.hive.jdbc.HiveDriver";
+                break;
+        }
+        return str;
+    }
+}
