fc09f29cee228a25e69caf00cb3768b0ff0a9dd3
francis <francis@francisdu.com>
2019-06-11 11:00:18 +0800

    Add PageGeroge

--- src/main/java/com/acquisition/controller/DatalakeTableStatusContrller.java
+++ src/main/java/com/acquisition/controller/DatalakeTableStatusContrller.java
@@ -2,10 +2,13 @@ package com.acquisition.controller;
 
 import com.acquisition.entity.CjDataSourceTabInfo;
 import com.acquisition.entity.Page;
+import com.acquisition.entity.PageGeorge;
 import com.acquisition.service.ICjDataSourceTabInfoService;
 import com.acquisition.util.Result;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -53,12 +56,26 @@ public class DatalakeTableStatusContrller {
      * @return 返回筛选后的数据
      */
     @PostMapping(value = "/getStatusBySys")
-    public Result getStatusBySys(@RequestBody CjDataSourceTabInfo data){
+    public Result getStatusBySys(@RequestBody PageGeorge<CjDataSourceTabInfo> reqParams){
         Result result = new Result();
-        result.setData(iCjDataSourceTabInfoService.findSumOfTablesBySys(
-                data.getBusinessSystemNameShortName()));
+        PageHelper.startPage(reqParams.getPagenum(),reqParams.getPagesize());
+        PageInfo<CjDataSourceTabInfo> page = new PageInfo(
+                iCjDataSourceTabInfoService.findSumOfTablesBySys(reqParams.query.getBusinessSystemNameShortName()));
+        result.setData(page);
         result.setMsg("查询成功！！！");
         result.setCode(200);
         return  result;
+    }
+
+    /**
+     * 导出成excle
+     */
+    @PostMapping(value = "/exportExcle")
+    public Result exportExcle(){
+        Result result = new Result();
+        HSSFWorkbook webbook = new HSSFWorkbook();
+        HSSFSheet sheet = webbook.createSheet("入湖报告！！！");
+
+        return result;
     }
 }
